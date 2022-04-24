673e73cc50dd054012611a858ceae899a65355ce
jeetundaviya <jeet.undaviya20017@gmail.com>
2022-04-24 10:19:55 +0530

    keyword based onion fetching added !

--- fresh_onions.py
+++ fresh_onions.py
@@ -29,6 +29,53 @@ def parse_table(page_source):
         url,title,added,last_check = tr.find_all('td')
         pt.add_row([url.text,title.text,added.text,last_check.text,status])
 
+def fetch_fresh_onions():
+
+    global d,pt
+
+    d.get(URL)
+
+    flds = [th.text for th in d.find_elements(By.TAG_NAME,'th')]
+    flds.append('Status')
+
+    pt = PrettyTable(field_names=flds)
+
+    pages = d.find_element(By.CLASS_NAME,'pageslct').find_elements(By.TAG_NAME,'a')
+    pages = [page.get_attribute('href') for page in pages]
+
+    print(f'[+] Getting results from {1} page. [+] Total links : {len(pt.rows)}')
+
+    parse_table(d.page_source)
+
+    for page in pages[1:]:
+        d.get(URL)
+        parse_table(d.page_source)
+
+        print(f'[+] Getting results from {pages.index(page)+1} page. [+] Total links : {len(pt.rows)}')
+    
+    with open(os.path.join(FRESH_ONIONS_RESULTS_SAVE_TO_PATH,'fresh_onions.csv'),'w') as f:
+        f.write(pt.get_csv_string())
+
+def fetch_fresh_onions_for_keyword(Keyword):
+
+    global d,pt
+
+    d.get(URL_WITH_QUERY+Keyword)
+
+    flds = [th.text for th in d.find_elements(By.TAG_NAME,'th')]
+    flds.append('Status')
+
+    pt = PrettyTable(field_names=flds)
+
+    print(f'[+] Getting results for {Keyword}.')
+
+    parse_table(d.page_source)
+    
+    print(f'[+] Total {len(pt.rows)} results fetched.')
+
+    with open(os.path.join(FRESH_ONIONS_RESULTS_SAVE_TO_PATH,f'fresh_onions_for_{Keyword}.csv'),'w') as f:
+        f.write(pt.get_csv_string())
+
 options = Options()
 options.add_argument('-headless')
 options.add_argument(f'user-agent={LATEST_CHROME_USERAGENT}')
@@ -36,29 +83,10 @@ options.add_argument(f'--proxy-server={TOR_PROXY}')
 
 d = Chrome(options=options)
 
-d.get(URL)
+pt = PrettyTable()
 
-flds = [th.text for th in d.find_elements(By.TAG_NAME,'th')]
-flds.append('Status')
+fetch_fresh_onions()
 
-pt = PrettyTable(field_names=flds)
-
-pages = d.find_element(By.CLASS_NAME,'pageslct').find_elements(By.TAG_NAME,'a')
-pages = [page.get_attribute('href') for page in pages]
-
-print(f'[+] Getting results from {1} page. [+] Total links : {len(pt.rows)}')
-
-parse_table(d.page_source)
-
-for page in pages[1:]:
-    d.get(URL)
-    parse_table(d.page_source)
-
-    print(f'[+] Getting results from {pages.index(page)+1} page. [+] Total links : {len(pt.rows)}')
+fetch_fresh_onions_for_keyword('hacking')
 
 d.quit()
-
-# print(pt.get_csv_string())
-
-with open(os.path.join(FRESH_ONIONS_RESULTS_SAVE_TO_PATH,'fresh_onions.csv'),'w') as f:
-    f.write(pt.get_csv_string())
