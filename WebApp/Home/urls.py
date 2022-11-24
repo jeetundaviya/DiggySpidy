de4b47703112f83a193ef209401d84f87383d855
Jeet Undaviya <jeet.undaviya20017@gmail.com>
2022-11-25 03:57:48 +0530

    Downloading Excel file Added (Now Downloading properly )

--- WebApp/Home/urls.py
+++ WebApp/Home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("",views.home,name="home"),
     path("home/",views.home,name="home"),
     path("dashboard/",views.dashboard,name="dashboard"),
-]
+    path("download/",views.download,name="download"),
+]   
--- WebApp/Home/views.py
+++ WebApp/Home/views.py
@@ -2,10 +2,15 @@ from DS_Core.diggy_spidy import ds_obj
 from django.shortcuts import render
 import pandas as pd
 import os
+import mimetypes
+# Import HttpResponse module
+from django.http.response import HttpResponse
+
+DATA_DICT = None
 
 # Create your views here.
 def home(response):
-    
+    global DATA_DICT     
     if response.method == "POST":
 
         link = response.POST.get('link')
@@ -17,8 +22,10 @@ def home(response):
 
         only_url = link.replace("https","").replace("http","").replace("://","")
 
-        data_dict = ds_obj.scrap(url=link)
+        DATA_DICT = ds_obj.scrap(url=link)
         
+        data_dict = DATA_DICT.copy()
+
         if data_dict:
             
             data_dict.pop("folder_location")
@@ -42,4 +49,26 @@ def home(response):
 
 def dashboard(response):
     return render(response,"dashboard.html",{})
-    
+
+def download(request):
+    global DATA_DICT
+    if DATA_DICT['excel_file_location'] != '':
+        
+        # Open the file for reading content
+        path = open(DATA_DICT['excel_file_location'] , 'rb')
+        # Set the mime type
+        mime_type, _ = mimetypes.guess_type(DATA_DICT['excel_file_location'])
+        # Set the return value of the HttpResponse
+        response = HttpResponse(path, content_type=mime_type)
+        # Set the HTTP header for sending to browser
+
+        link = ds_obj.base_url
+
+        only_url = link.replace("https","").replace("http","").replace("://","")
+
+        response['Content-Disposition'] = "attachment; filename=%s" % only_url+".xlsx"
+        # Return the response value
+        return response
+    else:
+        # Load the template
+        return render(request, 'home.html')
