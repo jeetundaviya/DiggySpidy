537f28ab703fa5f3eb8b8d2ceff88e246c7768c0
Jeet <jeet.undaviya20017@gmail.com>
2022-12-19 00:50:40 +0530

    Improved the form and added mapping for model

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class ScrapingSettings(models.Model):
+    feature = models.CharField(max_length=100)
+    enable = models.BooleanField(default=False)
--- Home/views.py
+++ Home/views.py
@@ -9,15 +9,24 @@ import shutil
 # Import HttpResponse module
 from django.http.response import HttpResponse
 
+from .models import ScrapingSettings
+
 DATA_DICT = None
 
 # Create your views here.
 def home(response):
     global DATA_DICT     
+
+    slow_mode = ScrapingSettings.objects.get(feature='slow')
+
+    torrify = ScrapingSettings.objects.get(feature='torrify')
+
     if response.method == "POST":
         link = response.POST.get('link')
 
         do_render_html = response.POST.get('do_render_html')
+        
+        enable_tor_proxy = response.POST.get('enable_tor_proxy')
         
         print(f'[+] do_render_html :- {do_render_html}')
 
@@ -28,6 +37,8 @@ def home(response):
 
         ds_obj.is_slow_mode = True if do_render_html else False
 
+        ds_obj.must_torrify = True if enable_tor_proxy else False
+
         only_url = link.replace("https","").replace("http","").replace("://","")
 
         DATA_DICT = ds_obj.scrap(url=link)
@@ -36,7 +47,14 @@ def home(response):
             
             data_dict = DATA_DICT.copy()
 
+            slow_mode.enable = True if do_render_html else False
+            slow_mode.save()
+
+            torrify.enable = True if enable_tor_proxy else False
+            torrify.save()
+
             if do_render_html:
+                
                 if data_dict["screenshot_location"] != "NA":
                     print(f'[+] Screenshot successfully found for {link}')
                     shutil.copyfile(data_dict["screenshot_location"],os.path.join("Home",os.path.join("static",os.path.join("assets","latest_ss.png"))))
@@ -55,15 +73,15 @@ def home(response):
 
             DATA_DICT = None # re-setting its value
 
-            return render(response,"home.html",{"Data_Dictonary":data_dict,"Only_URL":only_url ,"URL":link,"Table":html_table,"Excel_File_Location":excel_file_location})
+            return render(response,"home.html",{"Data_Dictonary":data_dict,"Only_URL":only_url ,"URL":link,"Table":html_table,"Excel_File_Location":excel_file_location,"slow_mode": slow_mode.enable ,"torrify": torrify.enable})
         else:
             DATA_DICT = None # re-setting its value
             print(f'[-] Error :- {ds_obj.errors}')
             error_msg = f"Failed scrape the {link} due to {'[->]'.join(ds_obj.errors)}"
-            return render(response,"home.html",{"ERROR_MSG":error_msg})
+            return render(response,"home.html",{"ERROR_MSG":error_msg,"slow_mode": slow_mode.enable ,"torrify": torrify.enable})
     else:
         DATA_DICT = None # re-setting its value
-        return render(response,"home.html",{})
+        return render(response,"home.html",{"slow_mode": slow_mode.enable ,"torrify": torrify.enable})
 
 def dashboard(response):
     return render(response,"dashboard.html",{})
